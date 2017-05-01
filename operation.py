f88acf5662d71d15ce054e1cf1b0b65044308e87
tian zhang <279736390@qq.com>
2017-05-01 22:07:27 +0800

    修改get_element函数 *args 参数传递错误

--- operation.py
+++ operation.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
@@ -8,53 +9,51 @@ import logging
 
 class Operation(object):
     def __init__(self, driver):
-        #self.driver = WebDriver
+        # self.driver = WebDriver
         self.driver = driver
 
-####################### For Test Action################
+    ####################### For Test Action################
     def open_page(self, url_):
         self.driver.get(url_)
-        self.driver.maximize_window()
+        # self.driver.maximize_window()
         logging.info("Open page:{0}".format(url_))
 
     def input_value(self, value, *loc):
         self.get_element(*loc).send_keys(value)
         logging.info("Input value: {0}".format(value))
 
-    def click(self, *loc):
-        self.get_element(*loc).click()
+    def click(self, loc):
+        self.get_element(loc).click()
         logging.info("Click element: {0} {1}".format(*loc))
 
-
-
-####################### For Expected Result Validation################
+    ####################### For Expected Result Validation################
     def verify(self, value, property, *loc):
         actual_result = None
 
         if property.upper() == "TEXT":
             actual_result = self.get_element(*loc).text
-        elif property.upper() == "":
-            pass
-
+        elif property.upper() == "VALUE":
+            actual_result = self.get_element(*loc).get_attribute(property)
         else:
             raise Exception
 
-        if actual_result == value:
+        if actual_result.encode("utf-8") == value:
             logging.info("Get string: {0}".format(value))
         else:
             assert False, logging.error("Get wrong value:{0}. Expected:{1}".format(actual_result, value))
 
-    def compare(self,):
+    def compare(self, ):
         pass
 
-
-####################### For Public Function################
-    def get_element(self, *loc):
+    ####################### For Public Function################
+    def get_element(self, *loc):  # loc <tuple>: (by,value)
         try:
             # 注意：以下入参为元组的元素，需要加*。Python存在这种特性，就是将入参放在元组里。
             # WebDriverWait(self.driver,10).until(lambda driver: driver.find_element(*loc).is_displayed())
             # 注意：以下入参本身是元组，不需要加*
-            WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(loc))
-            return self.driver.find_element(*loc)
+            WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(*loc))
+            return self.driver.find_element(**dict(zip(("by", "value"), *loc)))
         except NoSuchElementException:
             assert False, "Fail to find element: {0} {1}".format(*loc)
+
+
