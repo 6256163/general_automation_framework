1e6c44968ba981501a351634fdc0d26cd35ecb25
tian_zhang <tian_zhang@quantone.com>
2017-05-02 18:14:08 +0800

    添加Compare关键词。调整verify关键词

--- operation.py
+++ operation.py
@@ -1,13 +1,15 @@
 # coding=utf-8
+import logging
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.chrome.webdriver import WebDriver
-import logging
+from analysis import Analysis
 
 
 class Operation(object):
+
     def __init__(self, driver):
         # self.driver = WebDriver
         self.driver = driver
@@ -18,42 +20,55 @@ class Operation(object):
         # self.driver.maximize_window()
         logging.info("Open page:{0}".format(url_))
 
-    def input_value(self, value, *loc):
-        self.get_element(*loc).send_keys(value)
+    def input_value(self, value, loc):
+        self.get_element(loc).clear()
+        self.get_element(loc).send_keys(value)
         logging.info("Input value: {0}".format(value))
 
     def click(self, loc):
         self.get_element(loc).click()
-        logging.info("Click element: {0} {1}".format(*loc))
+        logging.info("Click element: {0} {1}".format(loc['by'], loc['value']))
+
+    def switch_to_frame(self, loc):
+        pass
 
     ####################### For Expected Result Validation################
-    def verify(self, value, property, *loc):
-        actual_result = None
+    def verify(self, value, property, loc):
+        actual_result = self.get_property_value(property, loc)
 
-        if property.upper() == "TEXT":
-            actual_result = self.get_element(*loc).text
-        elif property.upper() == "VALUE":
-            actual_result = self.get_element(*loc).get_attribute(property)
-        else:
-            raise Exception
-
-        if actual_result.encode("utf-8") == value:
+        if actual_result == value:
             logging.info("Get string: {0}".format(value))
         else:
             assert False, logging.error("Get wrong value:{0}. Expected:{1}".format(actual_result, value))
 
-    def compare(self, ):
-        pass
+    def compare(self, bys, locations, properties):
+        analysis = Analysis()
+        values = list()
+
+        for by, location, property in zip(bys, locations, properties):
+            loc = analysis.get_loc(by, location)
+            value = self.get_property_value(property,loc)
+            values.append(value)
+        for i in range(len(values)):
+            if values[0].lower() == values[i].lower():
+                pass
+            else:
+                assert False, logging.error("Compreation {0} are not equal".format(values))
+
 
     ####################### For Public Function################
-    def get_element(self, *loc):  # loc <tuple>: (by,value)
+    def get_element(self, loc):
+        if self.frame_driver != self.driver:
+            self.driver = self.frame_driver
         try:
-            # 注意：以下入参为元组的元素，需要加*。Python存在这种特性，就是将入参放在元组里。
-            # WebDriverWait(self.driver,10).until(lambda driver: driver.find_element(*loc).is_displayed())
-            # 注意：以下入参本身是元组，不需要加*
-            WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(*loc))
-            return self.driver.find_element(**dict(zip(("by", "value"), *loc)))
+            WebDriverWait(self.driver, 10).until(lambda driver: driver.find_element(**loc).is_displayed())
+            return self.driver.find_element(**loc)
         except NoSuchElementException:
-            assert False, "Fail to find element: {0} {1}".format(*loc)
+            assert False, "Fail to find element: {0} {1}".format(loc['by'], loc['value'])
 
 
+    def get_property_value(self,property, loc):
+        if property.upper() == "TEXT":
+            return self.get_element(loc).text
+        else:
+            return self.get_element(loc).get_attribute(property)
