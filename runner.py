c7374f05534c1de612ea8174eb45280be938e6e5
tian_zhang <tian_zhang@quantone.com>
2017-05-16 14:40:14 +0800

    优化执行逻辑，csv执行完毕之后退出浏览器

--- runner.py
+++ runner.py
@@ -15,24 +15,30 @@ from testcase import Testcase
 class Runner(object):
     def __init__(self):
         self.time = time.strftime('%Y-%m-%d %H-%M-%S', time.localtime(time.time()))
+
     def run(self):
         tc = Testcase()
         file_list = tc.get_csv_list()
         r = True
         tc_result = Result(self.time)
         for f in file_list:
-            logger = Logger(f,self.time).setup_logging()
+            logger = Logger(f, self.time).setup_logging()
+            steps = tc.get_steps(f)
+            csv_lines = tc.get_line(f)
+            #result init
             r = True
-            for step, csv_line in (zip(tc.get_steps(f), tc.get_line(f))):
+            exe = Execution(steps[0])
+            for step, line in (zip(steps, csv_lines)):
                 try:
-                    exe = Execution(step)
-                    logger.info(csv_line)
+                    logger.info(str(line))
+                    exe.update_step(step)
                     exe.execute()
                 except Exception as e:
                     r = False
                     logger.exception(e)
                     break
-            tc_result.set_result(f,r)
+            exe.quit()
+            tc_result.set_result(f, r)
         env = Environment(loader=PackageLoader("template_package", 'templates'))
         template = env.get_template('template.html')
 
