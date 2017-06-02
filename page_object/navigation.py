841a95b6b38aabd76ad9d6416b53ead85bc616cf
tian_zhang <tian_zhang@quantone.com>
2017-06-02 16:17:09 +0800

    优化菜单点击

--- page_object/navigation.py
+++ page_object/navigation.py
@@ -3,36 +3,31 @@ from selenium.webdriver.common.by import By
 
 
 class Navigation(BasePage):
-
     def __init__(self, driver):
         super(Navigation, self).__init__(driver)
 
-    office_account_management = (By.LINK_TEXT,u'公众号管理')
-    index = (By.LINK_TEXT,u'首页')
-    office_account_list = (By.LINK_TEXT,u'公众号列表')
-    fans_analysis = (By.LINK_TEXT,u'粉丝分析')
-    article_analysis = (By.LINK_TEXT,u'图文分析')
-    fans_ranking_list = (By.LINK_TEXT,u'粉丝排行')
-    article_ranking_list = (By.LINK_TEXT,u'文章排行')
+    office_account_management = (By.LINK_TEXT, u'公众号管理')
+    index = (By.LINK_TEXT, u'首页')
+    office_account_list = (By.LINK_TEXT, u'公众号列表')
+    fans_analysis = (By.LINK_TEXT, u'粉丝分析')
+    article_analysis = (By.LINK_TEXT, u'图文分析')
+    fans_ranking_list = (By.LINK_TEXT, u'粉丝排行')
+    article_ranking_list = (By.LINK_TEXT, u'文章排行')
 
-    task_management = (By.LINK_TEXT,u'任务管理')
-    task_material = (By.LINK_TEXT,u'任务素材')
-    task_statistics = ( By.LINK_TEXT, u'任务统计')
-    tag_management = ( By.LINK_TEXT, u'群组管理')
+    task_management = (By.LINK_TEXT, u'任务管理')
+    task_material = (By.LINK_TEXT, u'任务素材')
+    task_statistics = (By.LINK_TEXT, u'任务统计')
+    tag_management = (By.LINK_TEXT, u'群组管理')
 
-    console = (By.LINK_TEXT,u'控制台')
-    user_management = (By.LINK_TEXT,u'用户管理')
-    group_management = (By.LINK_TEXT,u'组织架构管理')
-
+    console = (By.LINK_TEXT, u'控制台')
+    user_management = (By.LINK_TEXT, u'用户管理')
+    group_management = (By.LINK_TEXT, u'组织架构管理')
 
     def click_menu(self, **kwargs):
         # input username
         if kwargs.get('menu', ''):
-            self.click(By.CSS_SELECTOR,'li.dropdown')
-            target_menu = getattr(Navigation, kwargs['menu'] )
-            self.click(*target_menu)
+            menus= kwargs['menu'].split('.')
+            for m in menus:
+                self.click(By.LINK_TEXT,m)
         else:
             assert False, "Username don't set"
-    
-
-
