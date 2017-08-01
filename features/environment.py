7116f533547d4de9095a39e3983d988c402be42b
tian_zhang <tian_zhang@quantone.com>
2017-08-01 18:49:50 +0800

    environment

---
+++ features/environment.py
@@ -0,0 +1,53 @@
+import os,time
+
+import sys
+
+from executer.execution import Execution
+from page_object.login import Login
+from page_object.navigation import Navigation
+
+parent_path = os.path.dirname(sys.path[0])
+if parent_path not in sys.path:
+    sys.path.append(parent_path)
+from page_object import store
+
+login_url = 'http://10.28.8.102/site/superentrance'
+
+
+def before_all(context):
+    """
+    
+    :param context: 
+    :return: 
+    
+    start_time = time.strftime('%Y-%m-%d %H-%M-%S', time.localtime(time.time()))
+    log_path = os.sep+'log'
+    if not os.path.exists(os.sep+'log'):
+        os.makedirs(log_path)
+    log_file = os.path.join(log_path, start_time.replace(' ','_')+'.log')
+    context.config.setup_logging(configfile=log_file)
+    """
+    # initialize the global store
+    store._init()
+    store.set_value("aaa", '111')
+
+
+
+def before_feature(context, feature):
+    # launch browser
+    if 'chrome' in feature.tags:
+        context.driver = Execution({'Browser': 'chrome'}).driver
+    # login user
+    context.login = Login(context.driver, url=login_url)
+    context.login.login(**{'username': '2', 'password': '123456', 'verifycode': 'imqa'})
+    context.navigation = Navigation(context.driver)
+    if feature.name == "Order":
+        context.navigation.click_menu(**{'menu':'order_list'})
+    elif feature.name == 'Price':
+        context.navigation.click_menu(**{'menu': 'price_list'})
+
+
+def after_feature(context, feature):
+    # close browser
+    context.driver.quit()
+
