2cd2d4b64f3929c02e97aa8dac1839b6fa075140
tian_zhang <tian_zhang@quantone.com>
2017-08-02 20:56:01 +0800

    before feature, load page module into context

--- features/environment.py
+++ features/environment.py
@@ -1,10 +1,13 @@
-import os,time
+import os, time
 
 import sys
 
 from executer.execution import Execution
 from page_object.login import Login
 from page_object.navigation import Navigation
+from page_object.page_object import PageObject
+from page_object.stock import Stock
+from page_object.table import Table
 
 parent_path = os.path.dirname(sys.path[0])
 if parent_path not in sys.path:
@@ -15,39 +18,34 @@ login_url = 'http://10.28.8.102/site/superentrance'
 
 
 def before_all(context):
-    """
-    
-    :param context: 
-    :return: 
-    
-    start_time = time.strftime('%Y-%m-%d %H-%M-%S', time.localtime(time.time()))
-    log_path = os.sep+'log'
-    if not os.path.exists(os.sep+'log'):
-        os.makedirs(log_path)
-    log_file = os.path.join(log_path, start_time.replace(' ','_')+'.log')
-    context.config.setup_logging(configfile=log_file)
-    """
     # initialize the global store
     store._init()
     store.set_value("aaa", '111')
-
 
 
 def before_feature(context, feature):
     # launch browser
     if 'chrome' in feature.tags:
         context.driver = Execution({'Browser': 'chrome'}).driver
+
     # login user
     context.login = Login(context.driver, url=login_url)
     context.login.login(**{'username': '2', 'password': '123456', 'verifycode': 'imqa'})
+
+    # Navigate to feature page
     context.navigation = Navigation(context.driver)
     if feature.name == "Order":
-        context.navigation.click_menu(**{'menu':'order_list'})
+        context.navigation.click_menu(**{'menu': 'order_list'})
     elif feature.name == 'Price':
         context.navigation.click_menu(**{'menu': 'price_list'})
+
+    # Load page module
+    page = '.'.join(['page_object', context.feature.name.lower(), context.feature.name])
+    context.operate = PageObject().get_instence(page)(context.driver)
+    context.table_ = Table(context.driver)
+    context.stock = Stock(context.driver)
 
 
 def after_feature(context, feature):
     # close browser
     context.driver.quit()
-
